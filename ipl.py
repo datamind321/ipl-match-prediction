32200e92ab4a3f9d9df73ea2786524a862aa1595
raj440singh <112772762+datamind321@users.noreply.github.com>
2023-04-24 14:54:51 +0530

    Update ipl.py

--- ipl.py
+++ ipl.py
@@ -3,13 +3,7 @@ import pickle
 import pandas as pd 
 import streamlit as st
 from PIL import Image 
-import sklearn
-from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+
 
 
 
